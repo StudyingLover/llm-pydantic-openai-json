5717c02415c1662fef8105e67939f4e81bb99e63
zjh <3046308220@qq.com>
2024-06-18 12:34:41 +0800

    update

--- main.py
+++ main.py
@@ -50,17 +50,17 @@ client = OpenAI(
 )
 
 item_desc = """
-这个物品是戒指，它非常受人欢迎，它的价格是1000.7美元，编号是123456，现在还有23个库存，他的位置在(1.0, 2.0, 3.0)，非常值得购买。
+这个物品是戒指,它非常受人欢迎,它的价格是1000.7美元,编号是123456,现在还有23个库存,他的位置在(1.0, 2.0, 3.0),非常值得购买。
 """
 
 user_prompt = f"""
-请帮我把这个物品的描述转换成json格式的数据，
-json scheme格式如下：
+请帮我把这个物品的描述转换成json格式的数据,
+json scheme格式如下:
 {Item.model_json_schema()}
-物品描述如下：
+物品描述如下:
 {item_desc}
-请你分析上面的描述，按照json schema，填写信息。请一定要按照json schema的格式填写，否则会导致数据无法解析，你会被狠狠地批评的。
-只需要输出可以被解析的json就够了，不需要输出其他内容。
+请你分析上面的描述,按照json schema,填写信息。请一定要按照json schema的格式填写,否则会导致数据无法解析,你会被狠狠地批评的。
+只需要输出可以被解析的json就够了,不需要输出其他内容。
 """
 
 resp = client.chat.completions.create(
@@ -68,14 +68,14 @@ resp = client.chat.completions.create(
     messages=[
         {
             "role": ChatRole.SYSTEM,
-            "content": "你是一个结构化数据的处理器，你精通json格式的数据，并且可以输出结构化的json数据。你可以根据给定的文字和json scheme，输出符合scheme的json数据。请注意，你的输出会直接被解析，如果格式不正确，会导致解析失败，你会被狠狠地批评的。",
+            "content": "你是一个结构化数据的处理器,你精通json格式的数据,并且可以输出结构化的json数据。你可以根据给定的文字和json scheme,输出符合scheme的json数据。请注意,你的输出会直接被解析,如果格式不正确,会导致解析失败,你会被狠狠地批评的。",
         },
         {"role": ChatRole.USER, "content": user_prompt},
     ],
 )
 
 item = Item.model_validate_json(extract_json(resp.choices[0].message.content))
-print(f"解析的物品信息：{item}")
+print(f"解析的物品信息:{item}")
 
 json_item = item.model_dump_json()
-print(f"转换成json格式：{json_item}")
+print(f"转换成json格式:{json_item}")
